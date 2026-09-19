ad8bc1693a147303080c8df9a89b08eaa2dd9fac
bender <bender@segonax.(none)>
2026-09-19 00:28:18 +0000

    Tweak compile flags and remove ram clearing code

--- loadram.c
+++ loadram.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
-#include"reu.h"
+#include"8086tiny.h"
 
 int main(){
   ram_init();
-
+#if 0
   puts("clear ram");
   for( uint32_t i = 0; i < 0x120000L; i+=2 ) {
     write_ram16( i, 0 );
   }
-
+#endif
   puts("loading bios");
   FILE *fp = fopen( "bios", "rb" );
   uint8_t val;
